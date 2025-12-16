9bcc33b283392ea0101662d214c5b6b514a7a59d
Dhananjaykumar2004 <dhananjayyadav8441@gmail.com>
2025-12-16 13:14:40 +0530

    Fix CORS for production domain

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://teezostore.vercel.app/",
-    "https://teezostore.com/"
+    "https://teezostore.com",
   ],
   credentials: true
 }))
